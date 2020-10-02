398116de8ecd37b9301ff82becd6e5d8de4601be
Nguyen Van Tuyen <vuongkhaituyen@Nguyens-MacBook-Pro.local>
2020-10-02 15:23:28 +0700

    add jumTo function

--- lib/screen/pick_date.dart
+++ lib/screen/pick_date.dart
@@ -14,17 +14,26 @@ class PickDate extends StatefulWidget {
 
 class _PickDateState extends State<PickDate> with TickerProviderStateMixin {
   Time time = Time();
-  List<String> _8hrs=["1hr","2hrs","3hrs","4hrs","5hrs","6hrs","7hrs","8hrs"];
+  List<String> _8hrs = [
+    "1hr",
+    "2hrs",
+    "3hrs",
+    "4hrs",
+    "5hrs",
+    "6hrs",
+    "7hrs",
+    "8hrs"
+  ];
   bool isVisibleHourList = false;
-  bool isVisibleDayList =true;
-  bool isVisibleMonthList=false;
-  bool isVisibleUntileButton =true;
+  bool isVisibleDayList = true;
+  bool isVisibleMonthList = false;
+  bool isVisibleUntileButton = true;
 
-  int _focusHourIndex=0;
-  int _focusDayIndex=0;
+  int _focusHourIndex = 0;
+  int _focusDayIndex = 0;
 
-    // Animation<Offset> _animation;
-    // AnimationController _controller;
+  // Animation<Offset> _animation;
+  // AnimationController _controller;
   @override
   void initState() {
     // TODO: implement initState
@@ -41,6 +50,7 @@ class _PickDateState extends State<PickDate> with TickerProviderStateMixin {
     //   curve: Curves.easeInCubic
     // ));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,29 +73,36 @@ class _PickDateState extends State<PickDate> with TickerProviderStateMixin {
                           color: Colors.white,
                           fontSize: 40),
                     ),
-                    isVisibleDayList ? Container(
-                        //color: Colors.white,
-                        height: 50,
-                        child: CustomScroll(
-                            inputText: time.getDayList(),
-                            callBack: (index) {
-                              _focusDayIndex=index;
-                              if (index != null && index != 0) {
-                                setState(() {
-                                  isVisibleHourList = true;
-                                });
-                              } else {
-                                setState(() {
-                                  isVisibleHourList = false;
-                                });
-                              }
-                            })): SizedBox(),
-                    isVisibleMonthList ? Container(
-                      //color: Colors.white,
-                        height: 50,
-                        child: CustomScroll(inputText: time.getMonthList(), callBack: (index) => (print(index)),)
-                    ):SizedBox(),
-                    isVisibleHourList ? Container(
+                    isVisibleDayList
+                        ? Container(
+                            //color: Colors.white,
+                            height: 50,
+                            child: CustomScroll(
+                                inputText: time.getDayList(),
+                                callBack: (index) {
+                                  _focusDayIndex = index;
+                                  if (index != null && index != 0) {
+                                    setState(() {
+                                      isVisibleHourList = true;
+                                    });
+                                  } else {
+                                    setState(() {
+                                      isVisibleHourList = false;
+                                    });
+                                  }
+                                }))
+                        : SizedBox(),
+                    isVisibleMonthList
+                        ? Container(
+                            //color: Colors.white,
+                            height: 50,
+                            child: CustomScroll(
+                              inputText: time.getMonthList(),
+                              callBack: (index) => (print(index)),
+                            ))
+                        : SizedBox(),
+                    isVisibleHourList
+                        ? Container(
                             //color: Colors.white,
                             height: 50,
                             child: CustomScroll(
@@ -93,40 +110,48 @@ class _PickDateState extends State<PickDate> with TickerProviderStateMixin {
                               callBack: (index) => (print(index)),
                             ))
                         : SizedBox(),
-                    isVisibleUntileButton ? Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        IconButton(
-                          icon: SvgPicture.asset("assets/icons/btn_plus.svg"),
-                          onPressed: () {
-                            print(_focusDayIndex);
-                            if(_focusDayIndex==0){
-                              setState(() {
-                                isVisibleDayList=false;
-                                isVisibleUntileButton =false;
-                              });
-                            }else{
-
-                            }
-                          },
-                        ),
-                        Text(
-                          "UNTIL",
-                          style: TextStyle(color: Colors.white),
-                        )
-                      ],
-                    ):SizedBox(),
-                    isVisibleDayList ? SizedBox() : Text("Now for", style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 40)
-                    ),
-                    isVisibleDayList ? SizedBox(): Container(
-                      height: 50,
-                      child: CustomScroll(inputText: _8hrs, callBack: (index){
-                        //print(index);
-                      },),
-                    )
+                    isVisibleUntileButton
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              IconButton(
+                                icon: SvgPicture.asset(
+                                    "assets/icons/btn_plus.svg"),
+                                onPressed: () {
+                                  print(_focusDayIndex);
+                                  if (_focusDayIndex == 0) {
+                                    setState(() {
+                                      isVisibleDayList = false;
+                                      isVisibleUntileButton = false;
+                                    });
+                                  } else {}
+                                },
+                              ),
+                              Text(
+                                "UNTIL",
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          )
+                        : SizedBox(),
+                    isVisibleDayList
+                        ? SizedBox()
+                        : Text("Now for",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 40)),
+                    isVisibleDayList
+                        ? SizedBox()
+                        : Container(
+                            height: 50,
+                            child: CustomScroll(
+                              inputText: _8hrs,
+                              callBack: (index) {
+                                //print(index);
+                              },
+                            ),
+                          )
                   ]),
             ),
             SizedBox(
